a8ecd00dfaf5ac747b35af2c4e6fa132e81a6613
Rafa Paradela <rafa.p@47deg.com>
2021-07-30 15:50:12 -0400

    adds a new endpoint

--- src/test/scala/githubalerts/HelloWorldSpec.scala
+++ src/test/scala/githubalerts/HelloWorldSpec.scala
@@ -1,7 +1,5 @@
 package githubalerts
 
-47deg.githubalertsscalarafap
-
 import cats.effect.IO
 import org.http4s._
 import org.http4s.implicits._
@@ -10,16 +8,21 @@ import munit.CatsEffectSuite
 class HelloWorldSpec extends CatsEffectSuite {
 
   test("HelloWorld returns status code 200") {
-    assertIO(retHelloWorld.map(_.status) ,Status.Ok)
+    assertIO(subscriptionEndpoint.map(_.status) ,Status.Ok)
   }
 
   test("HelloWorld returns hello world message") {
-    assertIO(retHelloWorld.flatMap(_.as[String]), "{\"message\":\"Hello, world\"}")
+    assertIO(subscriptionEndpoint.flatMap(_.as[String]), """[{"organization":"47deg","repository":"rafa","subscribedAt":"2021-04-15T12:30:15"}]""".stripMargin)
   }
 
-  private[this] val retHelloWorld: IO[Response[IO]] = {
-    val getHW = Request[IO](Method.GET, uri"/hello/world")
+
+  private[this] val subscriptionEndpoint: IO[Response[IO]] = {
+    val getHW = Request[IO](Method.GET, uri"/subscription/rafa")
     val helloWorld = HelloWorld.impl[IO]
-    GithubalertsscalarafapRoutes.helloWorldRoutes(helloWorld).orNotFound(getHW)
+    val jokes = new Jokes[IO] {
+      override def get: IO[Jokes.Joke] = IO.delay(Jokes.Joke("My Joke"))
+    }
+    val service = new SubscriptionService[IO](helloWorld, jokes)
+    GithubalertsscalarafapRoutes.appRoutes(service).orNotFound(getHW)
   }
 }
